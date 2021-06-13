6d0a8d26eb886158a8feeb02e79e51dc11b43397
Nazmul Hasan <nazmulhasan747406@gmail.com>
2021-06-13 15:13:11 +0600

    added main project

--- agami_bangla/wsgi.py
+++ agami_bangla/wsgi.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agami_bangla.settings')
 
 application = get_wsgi_application()
+django.setup()
