c1083cb1fb25d5417f09b444675d0fcc780c7f96
Nazmul Hasan <nazmulhasan747406@gmail.com>
2021-06-15 12:16:45 +0600

    adding new changes

--- agami_bangla/settings.py
+++ agami_bangla/settings.py
@@ -132,7 +132,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
